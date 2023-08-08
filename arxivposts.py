b1521a98516fd24e4c01084c77f471a2e966d65b
Amalie Stokholm <amaliestokholm@gmail.com>
2023-08-08 13:45:11 +0100

    Save blogposts in separate directory

--- arxivposts.py
+++ arxivposts.py
@@ -1,48 +1,53 @@
+"""
+This script writes a html blog post about a specific paper, written for isimba.dk
+"""
 import argparse
 import re
 from requests_html import HTMLSession
+
 session = HTMLSession()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('arxivid')
+parser.add_argument("arxivid")
+
 
 def main(arxivid):
-    arxivlink = 'https://arxiv.org/abs/' + arxivid
+    arxivlink = "https://arxiv.org/abs/" + arxivid
     r = session.get(arxivlink)
 
     # arxiv.org pages start with XML encoding declaration,
     # and requests_html does not like that. Remove the encoding declaration.
-    r.html.html = re.sub(r'^<\?xml.*?\?>', '', r.html.html)
+    r.html.html = re.sub(r"^<\?xml.*?\?>", "", r.html.html)
 
     # Now find your page, find the value you want to extract, inspect element
     # and copy selector and insert it in the string.
-    titles = r.html.find('#abs > h1')
-    titles, = titles
-    title = titles.text.replace('Title:', '')
+    titles = r.html.find("#abs > h1")
+    (titles,) = titles
+    title = titles.text.replace("Title:", "")
 
     # This finds the href blocks instantly
     # TODO: Map name in dictionary, so the right childs are read
     datadict = {}
-    for tr in r.html.find('#abs > div.metatable > table > tr'):
-        key, data = tr.find('td')
+    for tr in r.html.find("#abs > div.metatable > table > tr"):
+        key, data = tr.find("td")
         datadict[key.text] = data
 
     try:
-        pub = datadict['Comments:'].text
+        pub = datadict["Comments:"].text
     except KeyError:
         pub = ""
     try:
-        doi = datadict['DOI:'].text
-        doi = 'https://dx.doi.org/' + doi
+        doi = datadict["DOI:"].text
+        doi = "https://dx.doi.org/" + doi
     except:
         doi = arxivlink
 
-    abstracts, = r.html.find('#abs > blockquote')
-    abstract = abstracts.text.replace('Abstract: ', '')
+    (abstracts,) = r.html.find("#abs > blockquote")
+    abstract = abstracts.text.replace("Abstract: ", "")
 
     # Fix in-line math with MathJax by replacing $ with \(\).
-    c = abstract.count('$')
-    #assert c % 2 == 0
+    c = abstract.count("$")
+    # assert c % 2 == 0
 
     # Replace every other occurance with the opposite parantheses
     # https://stackoverflow.com/questions/46705546/python-replace-every-nth-occurrence-of-string
@@ -53,19 +58,18 @@ def main(arxivid):
         while find != -1:
             # if i  is equal to nth we found nth matches so replace
             if i == nth:
-                s = s[:find]+repl+s[find + len(sub):]
+                s = s[:find] + repl + s[find + len(sub) :]
                 i = 0
             # find + len(sub) + 1 means we start after the last match
             find = s.find(sub, find + len(sub) + 1)
             i += 1
         return s
 
-    abstract = nth_repl_all(abstract, '$', '\)', 2)
-    abstract = abstract.replace('$', '\([mathjax]', 1)
-    abstract = abstract.replace('$', '\(')
+    abstract = nth_repl_all(abstract, "$", "\)", 2)
+    abstract = abstract.replace("$", "\([mathjax]", 1)
+    abstract = abstract.replace("$", "\(")
 
-
-    tpl = '''<p style="text-align: justify;">{authors}</p>
+    tpl = """<p style="text-align: justify;">{authors}</p>
     <p style="text-align: left;">
     <strong><a class="link" href="{doi}"= target="_blank" rel="ContributionToJournal noopener">{title}</a>
     </strong> <a class="link" href="{arxiv}"= target="_blank" rel="ContributionToJournal noopener">See arXiv version</a>
@@ -76,30 +80,33 @@ def main(arxivid):
     <p></p>
     <h3 style="text-align: justify;"><strong>Abstract</strong></h3>
     <p style="text-align: justify;">{abstract}</p>
-    '''
+    """
 
     # typeset authors. if author is in isimba: boldface them.
-    authorss, = r.html.find('#abs > div.authors')
-    authors = authorss.find('a')
+    (authorss,) = r.html.find("#abs > div.authors")
+    authors = authorss.find("a")
     authorlist = []
     authortpl = '<a href="{href}">{name}</a>'
     for author in authors:
-        href = 'https://arxiv.org' + author.attrs['href']
-        a = authortpl.format(href=''.join(href.split()),
-                name=author.text.replace(',',''))
+        href = "https://arxiv.org" + author.attrs["href"]
+        a = authortpl.format(
+            href="".join(href.split()), name=author.text.replace(",", "")
+        )
         authorlist.append(a)
 
-    blogtext = tpl.format(authors=', '.join(authorlist),
-                          doi=doi,
-                          title=title,
-                          pub=pub,
-                          arxiv=arxivlink,
-                          abstract=abstract,
-                          )
+    blogtext = tpl.format(
+        authors=", ".join(authorlist),
+        doi=doi,
+        title=title,
+        pub=pub,
+        arxiv=arxivlink,
+        abstract=abstract,
+    )
     print(blogtext)
-    f = open('blogpost.txt', 'w')
-    f.write(blogtext)
-    f.close()
+    bp = open(f"./blogposts/{str(arxivid)}.txt", "w")
+    bp.write(blogtext)
+    bp.close()
+
 
 if __name__ == "__main__":
     main(**vars(parser.parse_args()))
